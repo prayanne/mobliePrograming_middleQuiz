42c87f15a5b539e6e8d87829b7903c20c0e99918
prayanne <kyhy0321@gmail.com>
2025-04-09 12:02:30 +0900

    addUtilActionBarMoreButton

--- app/src/main/java/com/example/mini_test_moblie/CalcActivity.kt
+++ app/src/main/java/com/example/mini_test_moblie/CalcActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mini_test_moblie
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,9 @@ class CalcActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
