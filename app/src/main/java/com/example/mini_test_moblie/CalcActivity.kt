ba33016fb15e34629a34d350e222af9d684cd2ae
prayanne <kyhy0321@gmail.com>
2025-04-09 11:14:45 +0900

    middleCheck

---
+++ app/src/main/java/com/example/mini_test_moblie/CalcActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mini_test_moblie
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CalcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_calc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
